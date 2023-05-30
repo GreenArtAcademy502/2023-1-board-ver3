24e3d9240fd97f0905b1a6f515101de6fb9c0e9d
박도흠 <pirbak@daum.net>
2023-05-30 09:56:35 +0900

    first

---
+++ src/main/java/com/green/boardver3/Boardver3Application.java
@@ -0,0 +1,13 @@
+package com.green.boardver3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boardver3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boardver3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/boardver3/Boardver3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.boardver3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boardver3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
